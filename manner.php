3d19eb7f46dd3eae4634aca9a6de80495de624ed
Jackson P <jacksonpauls@gmail.com>
2016-02-24 22:17:05 +0000

    Make array of sections.

--- manner.php
+++ manner.php
@@ -13,8 +13,8 @@ if (!is_file($filePath)) {
 
 $lines = file($filePath, FILE_IGNORE_NEW_LINES);
 
-$sectionNode = null;
-$subSectionNode = null;
+$sections = [];
+$sectionHeading = null;
 
 foreach ($lines as $i => $line) {
 
@@ -33,13 +33,26 @@ foreach ($lines as $i => $line) {
     // Start a section
     if (preg_match('~^\.SH (.*)$~', $line, $matches)) {
         $sectionHeading = $matches[1];
+        if (empty($sectionHeading)) {
+            exit($line . ' - empty section heading.');
+        }
+        $sections[$sectionHeading] = [];
         continue;
     }
 
-    // FAIL on unknown command
-    if (preg_match('~^\.~', $line, $matches)) {
-        exit($line . ' (' . $i . ')' . "\n");
+    // FAIL Got something and we're not in a section
+    if (is_null($sectionHeading)) {
+        exit($line . ' - not in a section.');
     }
 
-    echo $i, ' - ', $line, "\n";
+    $sections[$sectionHeading][] = $line;
+
+    // FAIL on unknown command
+//    if (preg_match('~^\.~', $line, $matches)) {
+//        exit($line . ' (' . $i . ')' . "\n");
+//    }
+
+//    echo $i, ' - ', $line, "\n";
 }
+
+var_dump($sections);
