29433329553a0f4506fbefa4a9f9e7eaa4a3ca01
Jackson P <jacksonpauls@gmail.com>
2016-03-22 15:08:17 +0000

    Handle .HP like a normal paragraph.

--- lib/Blocks.php
+++ lib/Blocks.php
@@ -19,7 +19,7 @@ class Blocks
             $line = $parentSectionNode->manLines[$i];
 
             // empty lines cause a new para also, see sar.1
-            if (preg_match('~^\.[LP]?P$~u', $line) || preg_match('~^\.sp$~u', $line)) {
+            if (preg_match('~^\.[HLP]?P$~u', $line) || preg_match('~^\.sp$~u', $line)) {
                 ++$blockNum;
                 $blocks[$blockNum] = $dom->createElement('p');
                 continue;
