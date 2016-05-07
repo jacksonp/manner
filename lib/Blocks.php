e7535022a720674aa5fbd032b90d5d779929a142
Jackson P <jacksonpauls@gmail.com>
2016-05-07 11:00:14 +0100

    Cope with case where .TE is preceeded by _ or =.

--- lib/Blocks.php
+++ lib/Blocks.php
@@ -372,94 +372,91 @@ class Blocks
                         if ($tr) {
                             $tr->setAttribute('class', 'border-bottom');
                         }
-                        $line = $blockNode->manLines[++$i];
-                        continue;
                     } elseif ($line === '=') {
                         if ($tr) {
                             $tr->setAttribute('class', 'border-bottom-double');
                         }
-                        $line = $blockNode->manLines[++$i];
-                        continue;
-                    }
-                    $tr   = $table->appendChild($dom->createElement('tr'));
-                    $cols = explode($columnSeparator, $line);
+                    } else {
+                        $tr   = $table->appendChild($dom->createElement('tr'));
+                        $cols = explode($columnSeparator, $line);
 
-                    for ($j = 0; $j < count($cols); ++$j) { // NB: $cols can get more elements with T{...
-                        if (isset($rowFormats[$tableRowNum])) {
-                            $thisRowFormat = $rowFormats[$tableRowNum];
-                            if (is_string($thisRowFormat) && $thisRowFormat === '---') {
-                                $tr->setAttribute('class', 'border-top');
-                                $thisRowFormat = @$rowFormats[$tableRowNum + 1];
-                            }
-                        }
-
-                        $tdClass = @$thisRowFormat[$j];
-
-                        // Ignore for now:
-                        // * equal-width columns
-                        $tdClass = str_replace(['e', 'E'], '', $tdClass);
-
-                        $tdClass = preg_replace('~[fF]?[bB]~', '', $tdClass, -1, $numReplaced);
-                        $bold    = $numReplaced > 0;
-
-                        if ($tableRowNum === 0 && $bold) {
-                            $cell = $dom->createElement('th');
-                        } else {
-                            $cell = $dom->createElement('td');
-                            if ($bold) {
-                                $tdClass = trim($tdClass . ' bold');
-                            }
-                        }
-                        if (!empty($tdClass)) {
-                            $cell->setAttribute('class', $tdClass);
-                        }
-                        $colspan = 1;
-                        for ($k = $j + 1; $k < count($thisRowFormat); ++$k) {
-                            if (@$thisRowFormat[$k] === 's') {
-                                ++$colspan;
-                            } else {
-                                break;
-                            }
-                        }
-                        if ($colspan > 1) {
-                            $cell->setAttribute('colspan', $colspan);
-                        }
-
-                        $tdContents = $cols[$j];
-
-                        if ($tdContents === '_') {
-                            $cell->appendChild($dom->createElement('hr'));
-                        } elseif (mb_strpos($tdContents, 'T{') === 0) {
-                            $tBlockLines = [];
-                            if (mb_strlen($tdContents) > 2) {
-                                $tBlockLines[] = mb_substr($tdContents, 2);
-                            }
-                            for ($i = $i + 1; $i < $numLines; ++$i) {
-                                $tBlockLine = $blockNode->manLines[$i];
-                                if (mb_strpos($tBlockLine, 'T}') === 0) {
-                                    if (mb_strlen($tBlockLine) > 2) {
-                                        $restOfLine = mb_substr($tBlockLine, 3); // also take out separator
-                                        $cols       = array_merge($cols, explode($columnSeparator, $restOfLine));
-                                    }
-                                    $cell->manLines = $tBlockLines;
-                                    self::handle($cell);
-                                    break;
-                                } else {
-                                    $tBlockLines[] = $tBlockLine;
+                        for ($j = 0; $j < count($cols); ++$j) { // NB: $cols can get more elements with T{...
+                            if (isset($rowFormats[$tableRowNum])) {
+                                $thisRowFormat = $rowFormats[$tableRowNum];
+                                if (is_string($thisRowFormat) && $thisRowFormat === '---') {
+                                    $tr->setAttribute('class', 'border-top');
+                                    $thisRowFormat = @$rowFormats[$tableRowNum + 1];
                                 }
                             }
 
-                        } else {
-                            TextContent::interpretAndAppendCommand($cell, $tdContents);
+                            $tdClass = @$thisRowFormat[$j];
+
+                            // Ignore for now:
+                            // * equal-width columns
+                            $tdClass = str_replace(['e', 'E'], '', $tdClass);
+
+                            $tdClass = preg_replace('~[fF]?[bB]~', '', $tdClass, -1, $numReplaced);
+                            $bold    = $numReplaced > 0;
+
+                            if ($tableRowNum === 0 && $bold) {
+                                $cell = $dom->createElement('th');
+                            } else {
+                                $cell = $dom->createElement('td');
+                                if ($bold) {
+                                    $tdClass = trim($tdClass . ' bold');
+                                }
+                            }
+                            if (!empty($tdClass)) {
+                                $cell->setAttribute('class', $tdClass);
+                            }
+                            $colspan = 1;
+                            for ($k = $j + 1; $k < count($thisRowFormat); ++$k) {
+                                if (@$thisRowFormat[$k] === 's') {
+                                    ++$colspan;
+                                } else {
+                                    break;
+                                }
+                            }
+                            if ($colspan > 1) {
+                                $cell->setAttribute('colspan', $colspan);
+                            }
+
+                            $tdContents = $cols[$j];
+
+                            if ($tdContents === '_') {
+                                $cell->appendChild($dom->createElement('hr'));
+                            } elseif (mb_strpos($tdContents, 'T{') === 0) {
+                                $tBlockLines = [];
+                                if (mb_strlen($tdContents) > 2) {
+                                    $tBlockLines[] = mb_substr($tdContents, 2);
+                                }
+                                for ($i = $i + 1; $i < $numLines; ++$i) {
+                                    $tBlockLine = $blockNode->manLines[$i];
+                                    if (mb_strpos($tBlockLine, 'T}') === 0) {
+                                        if (mb_strlen($tBlockLine) > 2) {
+                                            $restOfLine = mb_substr($tBlockLine, 3); // also take out separator
+                                            $cols       = array_merge($cols, explode($columnSeparator, $restOfLine));
+                                        }
+                                        $cell->manLines = $tBlockLines;
+                                        self::handle($cell);
+                                        break;
+                                    } else {
+                                        $tBlockLines[] = $tBlockLine;
+                                    }
+                                }
+
+                            } else {
+                                TextContent::interpretAndAppendCommand($cell, $tdContents);
+                            }
+                            $tr->appendChild($cell);
                         }
-                        $tr->appendChild($cell);
+                        ++$tableRowNum;
                     }
 
                     $line = $blockNode->manLines[++$i];
                     if (preg_match('~^\.TE~u', $line)) {
                         continue 2;
                     }
-                    ++$tableRowNum;
                 }
 
                 throw new Exception($line . ' - .TS without .TE in ' . $blocks[$blockNum]->tagName . ' at line ' . $i . '. Last line was "' . $blockNode->manLines[$i - 1] . '"');
