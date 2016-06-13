6499654dc2ad05c2cdcaa18b8a5d7122f4de6a4c
Jackson P <jacksonpauls@gmail.com>
2016-06-13 08:39:09 +0100

    Ignore .so for now so we can build builtins.1.

--- lib/Text.php
+++ lib/Text.php
@@ -309,7 +309,8 @@ class Text
             // .ps: affects point size
             // .bp: "Eject current page and begin new page."
             // .ul: "Underline (italicize in troff) N input lines." - Could revisit this and implement.
-            if (preg_match('~^[\.\'](iX|IX|nh|na|hy|UN|UC|DT|lf|TA|IN|LL|PU|LO 1|pl|pc|PD|RP|po|in|ll|fam|rs|rm|ta|cp|it|ps|bp|ul)~u',
+            // .so: ignore for now so we can build builtins.1
+            if (preg_match('~^[\.\'](iX|IX|nh|na|hy|UN|UC|DT|lf|TA|IN|LL|PU|LO 1|pl|pc|PD|RP|po|in|ll|fam|rs|rm|ta|cp|it|ps|bp|ul|so)~u',
               $line)) {
                 continue;
             }
