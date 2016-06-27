47bdf5958b38b993ce3b310f0dc23b1531d610d2
Jackson P <jacksonpauls@gmail.com>
2016-06-27 07:55:14 +0100

    Support .T&

--- lib/Block/TS.php
+++ lib/Block/TS.php
@@ -4,33 +4,15 @@
 class Block_TS
 {
 
-    static function checkAppend(HybridNode $parentNode, array $lines, int $i)
+    private static function parseRowFormats(array $lines, int $i): array
     {
 
-        if (!preg_match('~^\.TS~u', $lines[$i])) {
-            return false;
-        }
-
-        $dom      = $parentNode->ownerDocument;
         $numLines = count($lines);
-
-        $table = $dom->createElement('table');
-        $table->setAttribute('class', 'tbl');
-        $table = $parentNode->appendChild($table);
-
-        $columnSeparator = "\t";
-
-        $line = $lines[++$i];
-        if (mb_substr($line, -1, 1) === ';') {
-            if (preg_match('~tab\s?\((.)\)~', $line, $matches)) {
-                $columnSeparator = $matches[1];
-            }
-            $line = $lines[++$i];
-        }
 
         $rowFormats  = [];
         $formatsDone = false;
-        while ($i < $numLines - 1 && !$formatsDone) {
+        for (; $i < $numLines - 1; ++$i) {
+            $line = $lines[$i];
             if (mb_substr($line, -1, 1) === '.') {
                 $line        = rtrim($line, '.');
                 $formatsDone = true;
@@ -49,14 +31,52 @@ class Block_TS
                 }
                 $rowFormats[] = $colDefs;
             }
-            $line = $lines[++$i];
+
+            if ($formatsDone) {
+                break;
+            }
+
         }
+
+        return [$i + 1, $rowFormats];
+
+    }
+
+    static function checkAppend(HybridNode $parentNode, array $lines, int $i)
+    {
+
+        if (!preg_match('~^\.TS~u', $lines[$i])) {
+            return false;
+        }
+
+        $dom      = $parentNode->ownerDocument;
+        $numLines = count($lines);
+
+        $table = $dom->createElement('table');
+        $table->setAttribute('class', 'tbl');
+        $table = $parentNode->appendChild($table);
+
+        $columnSeparator = "\t";
+
+        $line = $lines[++$i];
+        if (mb_substr($line, -1, 1) === ';') {
+            if (preg_match('~tab\s?\((.)\)~u', $line, $matches)) {
+                $columnSeparator = $matches[1];
+            }
+            ++$i;
+        }
+
+        list($i, $rowFormats) = self::parseRowFormats($lines, $i);
 
         $tableRowNum = 0;
         $tr          = false;
 
         while ($i < $numLines - 1) {
-            if ($line === '_') {
+            $line = $lines[$i];
+            if ($line === '.T&') {
+                list($i, $rowFormats) = self::parseRowFormats($lines, $i + 1);
+                continue;
+            } elseif ($line === '_') {
                 if ($tr) {
                     $tr->setAttribute('class', 'border-bottom');
                 }
@@ -70,7 +90,7 @@ class Block_TS
                 $tr   = $table->appendChild($dom->createElement('tr'));
                 $cols = explode($columnSeparator, $line);
 
-                for ($j = 0; $j < count($cols); ++$j) { // NB: $cols can get more elements with T{...
+                for ($j = 0; $j < count($cols); ++$j) { // NB: $cols can grow more elements with T{...
                     if (isset($rowFormats[$tableRowNum])) {
                         $thisRowFormat = $rowFormats[$tableRowNum];
                         if (is_string($thisRowFormat) && $thisRowFormat === '---') {
@@ -85,10 +105,10 @@ class Block_TS
                     // * equal-width columns
                     $tdClass = str_replace(['e', 'E'], '', $tdClass);
 
-                    $tdClass = Replace::preg('~[fF]?[bB]~', '', $tdClass, -1, $numReplaced);
+                    $tdClass = Replace::preg('~[fF]?[bB]~u', '', $tdClass, -1, $numReplaced);
                     $bold    = $numReplaced > 0;
 
-                    if ($tableRowNum === 0 && $bold) {
+                    if ($tableRowNum === 0 and $bold) {
                         $cell = $dom->createElement('th');
                     } else {
                         $cell = $dom->createElement('td');
@@ -117,13 +137,13 @@ class Block_TS
                         $cell->appendChild($dom->createElement('hr'));
                     } elseif (mb_strpos($tdContents, 'T{') === 0) {
                         $tBlockLines = [];
-                        if (mb_strlen($tdContents) > 2) {
+                        if ($tdContents !== 'T{') {
                             $tBlockLines[] = mb_substr($tdContents, 2);
                         }
                         for ($i = $i + 1; $i < $numLines; ++$i) {
                             $tBlockLine = $lines[$i];
                             if (mb_strpos($tBlockLine, 'T}') === 0) {
-                                if (mb_strlen($tBlockLine) > 2) {
+                                if ($tBlockLine !== 'T}') {
                                     $restOfLine = mb_substr($tBlockLine, 3); // also take out separator
                                     $cols       = array_merge($cols, explode($columnSeparator, $restOfLine));
                                 }
