d71f992c96a5bb2ca3ab79501fc42853ee65e848
Jackson P <jacksonpauls@gmail.com>
2016-06-27 08:50:38 +0100

    Recursion in Roff_Condition.

--- lib/Roff/Condition.php
+++ lib/Roff/Condition.php
@@ -20,22 +20,43 @@ class Roff_Condition
         }
 
         ++$i;
+        $openBraces = 1;
+        $recurse    = false;
 
         for (; $i < $numLines; ++$i) {
             $line = $lines[$i];
-            if (preg_match('~^(.*)\\\\}$~u', $line, $matches)) {
+            $openBraces += substr_count($line, '\\{');
+            if ($openBraces > 1) {
+                $recurse = true;
+            }
+            $openBraces -= substr_count($line, '\\}');
+            if (preg_match('~^(.*)\\\\}$~u', $line, $matches) and $openBraces === 0) {
                 $foundEnd = true;
                 if (!empty($matches[1]) && $matches[1] !== '\'br') {
                     $replacementLines[] = Macro::massageLine($matches[1]);
                 }
                 break;
-            } elseif (!empty($line)) {
+            } elseif ($line !== '') {
                 $replacementLines[] = Macro::massageLine($line);
             }
         }
 
         if (!$foundEnd) {
             throw new Exception('.if n \\{ - not followed by expected pattern on line ' . $i . '.');
+        }
+
+        if ($recurse) {
+            $recurseLines = [];
+            for ($j = 0; $j < count($replacementLines); ++$j) {
+                $result = self::checkEvaluate($replacementLines, $j);
+                if ($result !== false) {
+                    $recurseLines = array_merge($recurseLines, $result[0]);
+                    $j            = $result[1];
+                } else {
+                    $recurseLines[] = $replacementLines[$j];
+                }
+            }
+            $replacementLines = $recurseLines;
         }
 
         return [$replacementLines, $i];
