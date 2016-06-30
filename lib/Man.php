116da8109340716d482dea669238edc1cb5c1cb0
Jackson P <jacksonpauls@gmail.com>
2016-06-30 15:40:10 +0100

    Set BD register to 0.

--- lib/Man.php
+++ lib/Man.php
@@ -48,12 +48,13 @@ class Man
           '.g' => '1',
             // Used by openpbs to specify -ms formatting (could remove and use 0 as fallback for undefined registers maybe):
           'Pb' => '0',
+          'BD' => '0',
             // F register != 0 used to signal we should generate index entries. See e.g. frogatto.6
           'F'  => '0',
             // horizontal position?
           'x'  => '0',
             // current line length
-          '.l' => '70'
+          '.l' => '70',
         ];
         $this->strings   = [
             // "The name of the current output device as specified by the -T command line option" (ps is default)
