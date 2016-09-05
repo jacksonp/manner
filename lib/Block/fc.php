53d4944de1a5595e6f6fddbc665b4ce0aa422a93
Jackson P <jacksonpauls@gmail.com>
2016-09-05 15:45:01 +0100

    Cope with tabs in .fc

--- lib/Block/fc.php
+++ lib/Block/fc.php
@@ -4,6 +4,17 @@
 class Block_fc
 {
 
+    private static function addRow(DOMDocument $dom, DOMElement $table, array $cells)
+    {
+        $tr = $dom->createElement('tr');
+        foreach ($cells as $contents) {
+            $td = $dom->createElement('td');
+            TextContent::interpretAndAppendText($td, $contents);
+            $tr->appendChild($td);
+        }
+        $table->appendChild($tr);
+    }
+
     static function checkAppend(HybridNode $parentNode, array $lines, int $i, array $arguments)
     {
 
@@ -21,20 +32,15 @@ class Block_fc
             } elseif (Request::is($line, 'fi')) {
                 break; // Finished
             } elseif (mb_strpos($line, $delim) === 0) {
-
-                $cells = preg_split('~' . preg_quote($delim, '~') . '~', $line);
+                $cells = preg_split('~' . preg_quote($delim, '~') . '~u', $line);
                 array_shift($cells);
-
-                $tr = $dom->createElement('tr');
-                foreach ($cells as $contents) {
-                    $contents = trim($contents, $pad);
-                    $td       = $dom->createElement('td');
-                    TextContent::interpretAndAppendText($td, $contents);
-                    $tr->appendChild($td);
-                }
-                $table->appendChild($tr);
-
-
+                $cells = array_map(function ($contents) use ($pad) {
+                    return trim($contents, $pad);
+                }, $cells);
+                self::addRow($dom, $table, $cells);
+            } elseif (mb_strpos($line, "\t") !== 0) {
+                $cells = preg_split("~\t~u", $line);
+                self::addRow($dom, $table, $cells);
             } else {
                 throw new Exception('Unexpected ' . $line);
             }
