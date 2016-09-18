56f04e1b454680b154573ea299a04dab0d2ebf2f
Jackson P <jacksonpauls@gmail.com>
2016-09-18 17:16:30 +0100

    Remove commented-out Blocks::handle()

--- lib/Blocks.php
+++ lib/Blocks.php
@@ -37,31 +37,6 @@ class Blocks
         return Block_TabTable::isStart($lines, $i);
     }
 
-    /*
-    static function handle(DOMElement $parentNode, array $lines)
-    {
-//        var_dump($parentNode->tagName);
-//        var_dump($lines);
-
-        $numLines = count($lines);
-        for ($i = 0; $i < $numLines; ++$i) {
-
-            $request = Request::getClass($lines, $i);
-
-            $newI = $request['class']::checkAppend($parentNode, $lines, $i, $request['arguments'], $request['request']);
-            if ($newI === false) {
-//            var_dump(array_slice($lines, $i - 5, 10));
-//            var_dump($lines);
-                throw new Exception('"' . $lines[$i] . '" Roff::parse() could not handle it.');
-            }
-
-            $i = $newI;
-
-        }
-
-    }
-    */
-
     static function _maybeLastEmptyChildWaitingForText(DOMElement $parentNode)
     {
         if (
