c3d5faea2133cf782a83538eeb34ae63afd46a4a
Jackson Pauls <jacksonpauls@gmail.com>
2017-01-22 20:44:38 +0000

    Fix to tabTable handling.

--- lib/Block/TabTable.php
+++ lib/Block/TabTable.php
@@ -6,6 +6,20 @@
 class Block_TabTable implements Block_Template
 {
 
+    const skippableLines = ['.br', ''];
+
+    // \&... see pmlogextract.1
+    const specialAcceptableLines = ['\\&...'];
+
+    private static function isTabTableLine($line)
+    {
+        $line = trim($line);
+        return
+            mb_strpos($line, "\t") !== false ||
+            in_array($line, self::skippableLines) ||
+            in_array($line, self::specialAcceptableLines);
+    }
+
     static function isStart($lines, $i)
     {
         // char before tab avoid indented stuff + exclude escaped tabs
@@ -15,10 +29,8 @@ class Block_TabTable implements Block_Template
             mb_strpos($lines[$i], "\t") > 0 &&
             preg_match('~[^\\\\\s]\t~u', $lines[$i]) &&
             (
-                (
-                    preg_match('~[^\\\\\s]\t~u', $lines[$i + 1]) && mb_strpos($lines[$i + 1], "\t") > 0) ||
-                (in_array(trim($lines[$i + 1]), ['.br', '', '\\&...'])
-                ) &&
+                (preg_match('~[^\\\\\s]\t~u', $lines[$i + 1]) && mb_strpos($lines[$i + 1], "\t") > 0) ||
+                in_array(trim($lines[$i + 1]), self::skippableLines + self::specialAcceptableLines) &&
                 preg_match('~[^\\\\\s]\t~u', $lines[$i + 2]) &&
                 mb_strpos($lines[$i + 2], "\t") > 0
             );
@@ -44,17 +56,19 @@ class Block_TabTable implements Block_Template
 
         while (count($lines)) {
 
-            if (mb_strpos($lines[0], "\t") === false && $lines[0] !== '\\&...') { // \&... see pmlogextract.1
+            $nextRequest = Request::getLine($lines, 0);
+
+            if (!self::isTabTableLine($nextRequest['raw_line'])) {
                 break;
             }
 
-            $line = array_shift($lines);
+            array_shift($lines);
 
-            if (in_array(trim($line), ['.br', ''])) {
+            if (in_array(trim($nextRequest['raw_line']), self::skippableLines)) {
                 continue;
             }
 
-            $tds = preg_split('~\t+~u', $line);
+            $tds = preg_split('~\t+~u', $nextRequest['raw_line']);
             $tr  = $table->appendChild($dom->createElement('tr'));
             foreach ($tds as $tdLine) {
                 $cell = $dom->createElement('td');
