bcfb56fcda037f7db59102e25caa1a09ee09713d
Jackson Pauls <jacksonpauls@gmail.com>
2017-01-28 15:45:59 +0000

    Added some tests.

---
+++ run_tests.php
@@ -0,0 +1,50 @@
+#!/usr/bin/env php
+<?php
+
+spl_autoload_register(function ($class) {
+    require_once 'lib/' . str_replace('_', '/', $class) . '.php';
+});
+
+function runTest($filePath)
+{
+    if (!is_file($filePath)) {
+        exit($filePath . ' is not a file.');
+    }
+
+    $expectedOutputPath = $filePath . '.html';
+
+    if (!is_file($expectedOutputPath)) {
+        exit($expectedOutputPath . ' is not a file.');
+    }
+
+    $fileLines = file($filePath, FILE_IGNORE_NEW_LINES);
+
+    ob_start();
+    Manner::roffToHTML($fileLines, $filePath);
+    $actualOutput = ob_get_contents();
+    ob_end_clean();
+
+    if ($actualOutput !== file_get_contents($expectedOutputPath)) {
+        echo $filePath, PHP_EOL;
+        echo '---------------------------', PHP_EOL;
+        echo implode(PHP_EOL, $fileLines), PHP_EOL;
+        echo '---------------------------', PHP_EOL;
+        echo 'Expected:', PHP_EOL;
+        echo file_get_contents($expectedOutputPath);
+        echo '---------------------------', PHP_EOL;
+        echo 'Got:', PHP_EOL;
+        echo $actualOutput, PHP_EOL;
+    }
+
+}
+
+$dir = new DirectoryIterator(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'tests');
+foreach ($dir as $fileInfo) {
+    if ($fileInfo->isDot()) {
+        continue;
+    }
+    if ($fileInfo->getExtension() === 'html') {
+        continue;
+    }
+    runTest($fileInfo->getRealPath());
+}
