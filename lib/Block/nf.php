454a1faa480cc556708005e75cd9db3cc370ec4a
Jackson Pauls <jacksonpauls@gmail.com>
2017-02-01 08:53:53 +0000

    Remove commented-out .nf handling.

--- lib/Block/nf.php
+++ lib/Block/nf.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class Block_nf implements Block_Template
 {
 
@@ -26,121 +25,6 @@ class Block_nf implements Block_Template
 
         return $pre;
 
-        /*
-
-    array_shift($lines);
-
-    $dom = $parentNode->ownerDocument;
-
-    $preLines = [];
-    while ($nextRequest = Request::getLine($lines)) {
-        if (Block_SS::endSubsection($nextRequest['request']) || $nextRequest['request'] === 'TS') {
-            break;
-        } elseif (self::endBlock($nextRequest)) {
-            array_shift($lines);
-            while (count($lines) && self::endBlock(Request::getLine($lines))) {
-                array_shift($lines); // swallow
-            }
-            break;
-        } elseif (
-            in_array($nextRequest['request'], ['EX', 'EE']) ||
-            ($nextRequest['request'] === 'ad' && $nextRequest['arg_string'] === 'l')
-        ) {
-            // Skip
-            array_shift($lines);
-        } else {
-            $preLines[] = array_shift($lines);
-        }
     }
-
-    if (count($preLines) === 0) {
-        return null;
-    }
-
-    if (
-        count($lines)
-        && Request::getLine($preLines)['request'] === 'RS'
-        && Request::getLine($lines)['request'] === 'RE'
-    ) {
-        $preLines[] = array_shift($lines);
-    }
-
-    if (count($preLines) === 0) {
-        return null;
-    }
-
-    if ($parentNode->tagName === 'p') {
-        $parentNode = $parentNode->parentNode;
-    }
-
-    if (count($preLines) > 1) {
-        $isTable = true;
-        foreach ($preLines as $preLine) {
-            $firstTab = mb_strpos($preLine, "\t");
-            if ($firstTab === false || $firstTab === 0) {
-                $isTable = false;
-                break;
-            }
-        }
-
-        if ($isTable) {
-            $table = $parentNode->appendChild($dom->createElement('table'));
-            foreach ($preLines as $preLine) {
-                if (in_array($preLine, ['.br', ''])) {
-                    continue;
-                }
-                $nextRequest = '';
-                // TODO: use Request?
-                if (mb_substr($preLine, 0, 1) === '.') {
-                    preg_match('~^(\.\w+ )"?(.*?)"?$~u', $preLine, $matches);
-                    $nextRequest = $matches[1];
-                    $preLine     = $matches[2];
-                }
-                $tds = preg_split('~\t+~u', $preLine);
-                $tr  = $table->appendChild($dom->createElement('tr'));
-                foreach ($tds as $tdLine) {
-                    $cell     = $dom->createElement('td');
-                    $codeNode = $cell->appendChild($dom->createElement('code'));
-                    if (empty($nextRequest)) {
-                        TextContent::interpretAndAppendText($codeNode, $tdLine);
-                    } else {
-                        $blockLines = [$nextRequest . $tdLine];
-                        Roff::parse($codeNode, $blockLines);
-                    }
-                    $tr->appendChild($cell);
-                }
-            }
-
-            return null;
-        }
-    }
-
-    $pre = $dom->createElement('pre');
-
-    $nextRequest = Request::getLine($preLines);
-    if ($nextRequest['request'] === 'RS') {
-        $lastRequest = Request::peepAt($preLines[count($preLines) - 1]);
-        if ($lastRequest['name'] === 'RE') {
-            array_pop($preLines);
-            array_shift($preLines);
-            $className = 'indent';
-            if (
-                !empty($nextRequest['arg_string']) &&
-                $indentVal = Roff_Unit::normalize(trim($nextRequest['arg_string'])) // note this filters out 0s
-            ) {
-                $className .= '-' . $indentVal;
-            }
-            $pre->setAttribute('class', $className);
-        }
-    }
-
-    BlockPreformatted::handle($pre, $preLines);
-    $parentNode->appendBlockIfHasContent($pre);
-
-    return $parentNode;
-
-        */
-    }
-
 
 }
