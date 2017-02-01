5c8a06ca5b4b5d54c0602e03a9ab18dc5a8b20ba
Jackson Pauls <jacksonpauls@gmail.com>
2017-02-01 13:57:56 +0000

    Remove commented-out .RS handling.

--- lib/Block/RS.php
+++ lib/Block/RS.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class Block_RS implements Block_Template
 {
 
@@ -16,7 +15,6 @@ class Block_RS implements Block_Template
 
         $dom = $parentNode->ownerDocument;
 
-//        $thisIndent = '';
         $className = 'indent';
         if (count($request['arguments']) > 0) {
             $thisIndent = Roff_Unit::normalize($request['arguments'][0]);
@@ -37,88 +35,7 @@ class Block_RS implements Block_Template
 
         $div = $parentNode->appendChild($div);
         return $div;
-        /*
-
-
-
-    $dom        = $parentNode->ownerDocument;
-    $skippedRSs = 0;
-
-    $rsLevel    = 1;
-    $blockLines = [];
-    while ($request = Request::getLine($lines)) {
-        if ($request['request'] === 'RS') {
-            $indent = Roff_Unit::normalize(trim($request['arg_string']));
-            if ($indent === $thisIndent) {
-                if (count($blockLines) > 0 && !in_array($blockLines[count($blockLines) - 1], ['.sp', '.br'])) {
-                    $blockLines[] = '.br';
-                }
-                ++$skippedRSs;
-                array_shift($lines);
-                continue;
-            } else {
-                ++$rsLevel;
-            }
-        } elseif ($request['request'] === 'RE') {
-            if ($skippedRSs > 0) {
-                --$skippedRSs;
-                array_shift($lines);
-                continue;
-            } else {
-                --$rsLevel;
-                if ($rsLevel === 0) {
-                    break;
-                }
-            }
-        } elseif ($request['request'] === 'TP') {
-            // prevent skipping
-            $thisIndent = 'GARBAGE';
-        } elseif (in_array($request['request'],  ['SH', 'SS'])) {
-            break;
-        }
-        $blockLines[] = array_shift($lines);
-    }
-
-    // Hack for duplicity.1
-//        if (count($blockLines) > 0 && $blockLines[count($blockLines) - 1] === '.PP') {
-//            $lines[$i] = '.PP';
-//            --$i;
-//        }
-
-    if ($parentNode->tagName === 'p') {
-        $parentNode = $parentNode->parentNode;
-    }
-
-    if (count($blockLines) > 0) {
-        $rsBlock = $dom->createElement('div');
-        $rsBlock->setAttribute('class', $className);
-        Roff::parse($rsBlock, $blockLines);
-        if ($className === 'indent' &&
-            $rsBlock->childNodes->length === 1 &&
-            $rsBlock->firstChild instanceof DOMElement &&
-            in_array($rsBlock->firstChild->tagName, ['dl', 'div'])
-        ) {
-            $parentNode->appendChild($rsBlock->firstChild);
-        } elseif ($className === 'indent' &&
-            $parentNode->tagName === 'dd'
-        ) {
-            $child = $rsBlock->firstChild;
-            while ($child) {
-                $nextSibling = $child->nextSibling;
-                $parentNode->appendBlockIfHasContent($child);
-                $child = $nextSibling;
-            }
-        } else {
-            $parentNode->appendBlockIfHasContent($rsBlock);
-
-        }
-    }
-
-    return $parentNode;
-
-        */
 
     }
-
 
 }
