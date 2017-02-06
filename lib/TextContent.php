daea51ac8e6816723e9fba5cbaea334681da0f7f
Jackson Pauls <jacksonpauls@gmail.com>
2017-02-06 21:40:11 +0000

    Code formatting.

--- lib/TextContent.php
+++ lib/TextContent.php
@@ -17,11 +17,11 @@ class TextContent
 
         if (!is_null($man->eq_delim_left) && !is_null($man->eq_delim_right)) {
             if (preg_match(
-              '~^(.*?)' .
-              preg_quote($man->eq_delim_left, '~') .
-              '(.+?)' .
-              preg_quote($man->eq_delim_right, '~') .
-              '(.*)$~', $line, $matches)
+                '~^(.*?)' .
+                preg_quote($man->eq_delim_left, '~') .
+                '(.+?)' .
+                preg_quote($man->eq_delim_right, '~') .
+                '(.*)$~', $line, $matches)
             ) {
                 if (mb_strlen($matches[1]) > 0) {
                     self::interpretAndAppendText($parentNode, $matches[1]);
@@ -59,7 +59,7 @@ class TextContent
 
                     if ($letterPosition > $lastLetterPosition + 1) {
                         self::interpretAndAppendText($parentNode,
-                          mb_substr($line, $lastLetterPosition, $letterPosition - $lastLetterPosition - 1));
+                            mb_substr($line, $lastLetterPosition, $letterPosition - $lastLetterPosition - 1));
                     }
 
                     if ($letter === 'u' && $nextLetter === 'd') {
@@ -68,7 +68,7 @@ class TextContent
                         $newChildNode = $parentNode->appendChild($dom->createElement('sub'));
                     }
                     self::interpretAndAppendText($newChildNode,
-                      mb_substr($line, $letterPosition + 1, $nextLetterPosition - $letterPosition - 2));
+                        mb_substr($line, $letterPosition + 1, $nextLetterPosition - $letterPosition - 2));
 
                     $lastLetterPosition = $nextLetterPosition + 1;
 
@@ -112,18 +112,18 @@ class TextContent
         self::$continuation = $replacements > 0;
 
         $textSegmentsS = preg_split(
-          '~(?<!\\\\)((?:\\\\\\\\)*)(\\\\[fF](?:[^\(\[]|\(..|\[.*?\])?|\\\\[ud]|\\\\k(?:[^\(\[]|\(..|\[.*?\]))~u',
-          $line,
-          -1,
-          PREG_SPLIT_DELIM_CAPTURE
+            '~(?<!\\\\)((?:\\\\\\\\)*)(\\\\[fF](?:[^\(\[]|\(..|\[.*?\])?|\\\\[ud]|\\\\k(?:[^\(\[]|\(..|\[.*?\]))~u',
+            $line,
+            -1,
+            PREG_SPLIT_DELIM_CAPTURE
         );
 
         $textSegments = [];
         for ($i = 0; $i < count($textSegmentsS); ++$i) {
             if ($textSegmentsS[$i] === '\\\\' &&
-              $i > 0 &&
-              count($textSegments) > 0 &&
-              mb_substr($textSegments[count($textSegments) - 1], 0, 1) !== '\\'
+                $i > 0 &&
+                count($textSegments) > 0 &&
+                mb_substr($textSegments[count($textSegments) - 1], 0, 1) !== '\\'
             ) {
                 $textSegments[count($textSegments) - 1] .= '\\\\';
             } elseif ($textSegmentsS[$i] !== '') {
@@ -147,8 +147,10 @@ class TextContent
                 continue;
             }
 
-            if ($i < $numTextSegments - 1 && in_array(mb_substr($textSegments[$i], 0, 2),
-                ['\f', '\F']) && in_array(mb_substr($textSegments[$i + 1], 0, 2), ['\f', '\F', '\d', '\u'])
+            if (
+                $i < $numTextSegments - 1 &&
+                in_array(mb_substr($textSegments[$i], 0, 2), ['\f', '\F']) &&
+                in_array(mb_substr($textSegments[$i + 1], 0, 2), ['\f', '\F', '\d', '\u'])
             ) {
                 continue;
             }
@@ -158,8 +160,7 @@ class TextContent
                     if ($i < $numTextSegments - 1) {
                         if ($i === $numTextSegments - 2 || $textSegments[$i + 2] === '\d') {
                             $sup = $parentNode->appendChild($dom->createElement('sup'));
-                            $sup->appendChild(new DOMText(
-                              self::interpretString($textSegments[++$i], false)));
+                            $sup->appendChild(new DOMText(self::interpretString($textSegments[++$i], false)));
                             ++$i;
                         } else {
                             // Do nothing - just drop the \u
@@ -170,8 +171,7 @@ class TextContent
                     if ($i < $numTextSegments - 1) {
                         if ($i === $numTextSegments - 2 || $textSegments[$i + 2] === '\u') {
                             $sub = $parentNode->appendChild($dom->createElement('sub'));
-                            $sub->appendChild(new DOMText(
-                              self::interpretString($textSegments[++$i], false)));
+                            $sub->appendChild(new DOMText(self::interpretString($textSegments[++$i], false)));
                             ++$i;
                         } else {
                             // Do nothing - just drop the \d
@@ -214,8 +214,7 @@ class TextContent
                     if ($i < $numTextSegments - 1) {
                         $strong = $parentNode->appendChild($dom->createElement('strong'));
                         $em     = $strong->appendChild($dom->createElement('em'));
-                        $em->appendChild(new DOMText(
-                          self::interpretString($textSegments[++$i])));
+                        $em->appendChild(new DOMText(self::interpretString($textSegments[++$i])));
                     }
                     break;
                 case '\f':
@@ -286,8 +285,7 @@ class TextContent
                 case '\f(SM':
                     if ($i < $numTextSegments - 1) {
                         $small = $parentNode->appendChild($dom->createElement('small'));
-                        $small->appendChild(new DOMText(
-                          self::interpretString($textSegments[++$i])));
+                        $small->appendChild(new DOMText(self::interpretString($textSegments[++$i])));
                     }
                     break;
                 default:
@@ -300,15 +298,15 @@ class TextContent
 
     }
 
-    static function interpretString(string $string, bool $applyCharTranslations = true):string
+    static function interpretString(string $string, bool $applyCharTranslations = true): string
     {
 
         $man = Man::instance();
 
         $string = Replace::pregCallback('~(?<!\\\\)(?<bspairs>(?:\\\\\\\\)*)\\\\N\'(?<charnum>\d+)\'~u',
-          function ($matches) {
-              return $matches['bspairs'] . chr((int)$matches['charnum']);
-          }, $string);
+            function ($matches) {
+                return $matches['bspairs'] . chr((int)$matches['charnum']);
+            }, $string);
 
         $roffStrings = $man->getStrings();
 
@@ -323,81 +321,81 @@ class TextContent
         // NB: these substitutions have to happen at the same time, with no backtracking to look again at replaced chars.
         $singleCharacterEscapes = [
             // "\e represents the current escape character." - let's hope it's always a backslash
-          'e'  => '\\',
+            'e' => '\\',
             // 1/6 em narrow space glyph, e.g. enigma.6 synopsis. Just remove for now (but don't do this earlier to not
             // break case where it's followed by a dot, e.g. npm-cache.1).
-          '|'  => '',
+            '|' => '',
             // 1/12 em half-narrow space glyph; zero width in nroff. Just remove for now.
-          '^'  => '',
+            '^' => '',
             // Default optional hyphenation character. Just remove for now.
-          '%'  => '',
+            '%' => '',
             // Inserts a zero-width break point (similar to \% but without a soft hyphen character). Just remove for now.
-          ':'  => '',
+            ':' => '',
             // Digit-width space.
-          '0'  => ' ',
+            '0' => ' ',
             // "To begin a line with a control character without it being interpreted, precede it with \&.
             // This represents a zero width space, which means it does not affect the output."
             // (also remove tho if not at start of line.)
             // This also is used in practice after a .TP to have indented text without a visible term in front.
-          '&'  => Char::ZERO_WIDTH_SPACE_UTF8,
+            '&' => Char::ZERO_WIDTH_SPACE_UTF8,
             // variation on \&
-          ')'  => '',
-          '\\' => '\\',
+            ')' => '',
+            '\\' => '\\',
 
             // stray block ends (e.g. pmieconf.1):
-          '}'  => '',
-          '{'  => '',
+            '}' => '',
+            '{' => '',
 
             // \/ Increases the width of the preceding glyph so that the spacing between that glyph and the following glyph is correct if the following glyph is a roman glyph. groff(7)
-          '/'  => '',
+            '/' => '',
             // \, Modifies the spacing of the following glyph so that the spacing between that glyph and the preceding glyph is correct if the preceding glyph is a roman glyph. groff(7)
-          ','  => '',
+            ',' => '',
             // The same as a dot (‘.’).  Necessary in nested macro definitions so that ‘\\..’ expands to ‘..’.
-          '.'  => '.',
-          '\'' => '´',
+            '.' => '.',
+            '\'' => '´',
             // The acute accent ´; same as \(aa.
-          '´'  => '´',
+            '´' => '´',
             // The grave accent `; same as \(ga.
-          '`'  => '`',
-          '-'  => '-',
+            '`' => '`',
+            '-' => '-',
             // The same as \(ul, the underline character.
-          '_'  => '_',
-          't'  => "\t",
+            '_' => '_',
+            't' => "\t",
             // Unpaddable space size space glyph (no line break). See enigma.6:
-          ' '  => mb_convert_encoding(chr(160), 'UTF-8', 'HTML-ENTITIES'),
+            ' ' => mb_convert_encoding(chr(160), 'UTF-8', 'HTML-ENTITIES'),
             // Unbreakable space that stretches like a normal inter-word space when a line is adjusted
-          '~'  => mb_convert_encoding(chr(160), 'UTF-8', 'HTML-ENTITIES'),
+            '~' => mb_convert_encoding(chr(160), 'UTF-8', 'HTML-ENTITIES'),
 
         ];
 
         $string = Replace::pregCallback(
-          '~(?J)(?<!\\\\)(?<bspairs>(?:\\\\\\\\)*)\\\\(\[(?<glyph>[^\]\s]+)\]|\((?<glyph>[^\s]{2})|\*\[(?<string>[^\]\s]+)\]|\*\((?<string>[^\s]{2})|\*(?<string>[^\s])|(?<char>.))~u',
-          function ($matches) use (&$singleCharacterEscapes, &$roffStrings) {
-              // \\ "reduces to a single backslash" - Do this first as strtr() doesn't search replaced text for further replacements.
-              $prefix = str_repeat('\\', mb_strlen($matches['bspairs']) / 2);
-              if ($matches['glyph'] !== '') {
-                  if (array_key_exists($matches['glyph'], Roff_Glyph::ALL_GLYPHS)) {
-                      return $prefix . Roff_Glyph::ALL_GLYPHS[$matches['glyph']];
-                  } else {
-                      return $prefix; // Follow what groff does, if string isn't set use empty string.
-                  }
-              } elseif ($matches['string'] !== '') {
-                  if (isset($roffStrings[$matches['string']])) {
-                      return $prefix . $roffStrings[$matches['string']];
-                  } else {
-                      return $prefix; // Follow what groff does, if string isn't set use empty string.
-                  }
-              } else {
-                  if (isset($singleCharacterEscapes[$matches['char']])) {
-                      return $prefix . $singleCharacterEscapes[$matches['char']];
-                  } else {
-                      // If a backslash is followed by a character that does not constitute a defined escape sequence,
-                      // the backslash is silently ignored and the character maps to itself.
-                      return $prefix . $matches['char'];
-                  }
-              }
-          },
-          $string);
+            '~(?J)(?<!\\\\)(?<bspairs>(?:\\\\\\\\)*)\\\\(\[(?<glyph>[^\]\s]+)\]|\((?<glyph>[^\s]{2})|\*\[(?<string>[^\]\s]+)\]|\*\((?<string>[^\s]{2})|\*(?<string>[^\s])|(?<char>.))~u',
+            function ($matches) use (&$singleCharacterEscapes, &$roffStrings) {
+                // \\ "reduces to a single backslash" - Do this first as strtr() doesn't search replaced text for further replacements.
+                $prefix = str_repeat('\\', mb_strlen($matches['bspairs']) / 2);
+                if ($matches['glyph'] !== '') {
+                    if (array_key_exists($matches['glyph'], Roff_Glyph::ALL_GLYPHS)) {
+                        return $prefix . Roff_Glyph::ALL_GLYPHS[$matches['glyph']];
+                    } else {
+                        return $prefix; // Follow what groff does, if string isn't set use empty string.
+                    }
+                } elseif ($matches['string'] !== '') {
+                    if (isset($roffStrings[$matches['string']])) {
+                        return $prefix . $roffStrings[$matches['string']];
+                    } else {
+                        return $prefix; // Follow what groff does, if string isn't set use empty string.
+                    }
+                } else {
+                    if (isset($singleCharacterEscapes[$matches['char']])) {
+                        return $prefix . $singleCharacterEscapes[$matches['char']];
+                    } else {
+                        // If a backslash is followed by a character that does not constitute a defined escape sequence,
+                        // the backslash is silently ignored and the character maps to itself.
+                        return $prefix . $matches['char'];
+                    }
+                }
+            },
+            $string);
 
         if ($applyCharTranslations) {
             $string = $man->applyCharTranslations($string);
