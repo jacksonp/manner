3cd3a6d7d8aabab8f02fd138f36ee7f7171195ad
Jackson Pauls <jacksonpauls@gmail.com>
2017-02-09 11:38:58 +0000

    Work on better .TS handling.

--- lib/Block/TS.php
+++ lib/Block/TS.php
@@ -4,6 +4,64 @@ declare(strict_types = 1);
 class Block_TS implements Block_Template
 {
 
+    private static $rowFormatKeyCharacters = [
+        'A',
+        'C',
+        'L',
+        'N',
+        'R',
+        '^',
+        '_',
+        '-',
+        '=',
+        '|'
+    ];
+
+    private static function getFormatChar(string $line, int $i): string
+    {
+        $char      = mb_substr($line, $i, 1);
+        $upperChar = mb_strtoupper($char);
+        return in_array($upperChar, self::$rowFormatKeyCharacters) ? $upperChar : $char;
+    }
+
+    private static function parseRowFormat(string $line): array
+    {
+        $formats    = [];
+        $format     = self::getFormatChar($line, 0);
+        $lineLength = mb_strlen($line);
+        for ($i = 1; $i < $lineLength; ++$i) {
+            $char = self::getFormatChar($line, $i);
+            if (in_array($char, self::$rowFormatKeyCharacters)) {
+                $formats[] = trim($format);
+                $format    = '';
+            }
+            $format .= $char;
+            if (in_array($char, ['f', 'F'])) {
+                // Either of these specifiers may be followed by a font name (either one or two characters long), font
+                // number (a single digit), or long name in parentheses (the last form is a GNU tbl extension). A
+                // one-letter font name must be separated by one or more blanks from whatever follows.
+                $charAfterF = self::getFormatChar($line, ++$i);
+                if (preg_match('~\d~', $charAfterF)) {
+                    $format .= $charAfterF;
+                } elseif (preg_match('~[A-Z]~', $charAfterF)) {
+                    $charAfterAfterF = self::getFormatChar($line, ++$i);
+                    $format .= $charAfterF . $charAfterAfterF;
+                } elseif ($charAfterF === '(') {
+                    $format .= $charAfterF;
+                    while ($i < $lineLength) {
+                        $nextChar = self::getFormatChar($line, ++$i);
+                        $format .= $nextChar;
+                        if ($nextChar === ')') {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        $formats[] = trim($format);
+        return $formats;
+    }
+
     private static function parseRowFormats(array &$lines): ?array
     {
 
@@ -19,19 +77,12 @@ class Block_TS implements Block_Template
                 $line        = rtrim($line, '.');
                 $formatsDone = true;
             }
-            $line = str_replace('|', '', $line);
+            // Ignore vertical bars for now:
+            $line = str_replace('|', ' ', $line);
             if (preg_match('~^[-_\s]+$~u', $line)) {
                 $rowFormats[] = '---';
             } else {
-                // Ignore vertical bars for now:
-                $colDefs = preg_split('~\s+~', $line);
-                if (count($colDefs) === 1) {
-                    $colDefs = str_split($colDefs[0]);
-                }
-                foreach ($colDefs as $k => $v) {
-                    $colDefs[$k] = strtr($v, ['l' => '', 'L' => '']);
-                }
-                $rowFormats[] = $colDefs;
+                $rowFormats[] = self::parseRowFormat($line);
             }
 
             if ($formatsDone) {
@@ -60,11 +111,24 @@ class Block_TS implements Block_Template
 
         $columnSeparator = "\t";
 
+        $tableClasses = ['tbl'];
+
         if (mb_substr(trim($lines[0]), -1, 1) === ';') {
-            if (preg_match('~tab\s?\((.)\)~u', $lines[0], $matches)) {
-                $columnSeparator = $matches[1];
-            }
+            $globalOptions = rtrim($lines[0], ';');
             array_shift($lines);
+            // Treat this separately as a space after "tab" is accepted:
+            Replace::pregCallback('~tab\s?\((.)\)~u', function ($matches) use (&$columnSeparator) {
+                $columnSeparator = $matches[1];
+                return '';
+            }, $globalOptions);
+            $globalOptions = preg_split('~[\\t ,]~', $globalOptions);
+            foreach ($globalOptions as $globalOption) {
+                if (in_array($globalOption, ['box'])) {
+                    $tableClasses[] = $globalOption;
+                } elseif ($globalOption === 'center') {
+                    $tableClasses[] = 'center-table';
+                }
+            }
         }
 
         $rowFormats = self::parseRowFormats($lines);
@@ -73,7 +137,7 @@ class Block_TS implements Block_Template
         }
 
         $table = $dom->createElement('table');
-        $table->setAttribute('class', 'tbl');
+        $table->setAttribute('class', implode(' ', $tableClasses));
         $tableRowNum  = 0;
         $formatRowNum = 0;
         $tr           = false;
@@ -110,25 +174,35 @@ class Block_TS implements Block_Template
             } elseif (!is_null($request['request']) && $request['request'] !== '') {
                 continue;
             } else {
-                $tr           = $dom->createElement('tr');
+                $tr = $dom->createElement('tr');
+
+                if (isset($rowFormats[$formatRowNum])) {
+                    $thisRowFormat = $rowFormats[$formatRowNum];
+                    if (is_string($thisRowFormat) && $thisRowFormat === '---') {
+                        if ($table->lastChild) {
+                            $table->lastChild->setAttribute('class', 'border-bottom');
+                        } else {
+                            $tr->setAttribute('class', 'border-top');
+                        }
+                        ++$formatRowNum;
+                        $thisRowFormat = @$rowFormats[$formatRowNum];
+                    }
+                }
+
                 $tr           = $table->appendChild($tr);
                 $cols         = explode($columnSeparator, $request['raw_line']);
                 $totalColSpan = 0;
 
                 for ($j = 0; $j < count($cols); ++$j) { // NB: $cols can grow more elements with T{...
-                    if (isset($rowFormats[$formatRowNum])) {
-                        $thisRowFormat = $rowFormats[$formatRowNum];
-                        if (is_string($thisRowFormat) && $thisRowFormat === '---') {
-                            $tr->setAttribute('class', 'border-top');
-                            $thisRowFormat = @$rowFormats[$formatRowNum + 1];
-                        }
-                    }
 
                     $tdClass = @$thisRowFormat[$totalColSpan];
 
                     // Ignore for now:
-                    // * equal-width columns
-                    $tdClass = str_replace(['e', 'E'], '', $tdClass);
+                    // * equal-width columns,
+                    // * setting the font to Regular
+                    $tdClass = str_replace(['e', 'E', 'f(R)'], '', $tdClass);
+
+                    $tdClass = str_replace(['f(CW)'], ' code ', $tdClass);
 
                     $tdClass = Replace::preg('~[fF]?[bB]~u', '', $tdClass, -1, $numReplaced);
                     if ($nextRowBold) {
@@ -147,21 +221,27 @@ class Block_TS implements Block_Template
                         }
                     }
 
-                    if (!empty($tdClass)) {
+                    $tdClass = Replace::preg('~^L(.*)$~', '$1', $tdClass);
+                    $tdClass = Replace::preg('~^C(.*)$~', 'center $1', $tdClass);
+                    $tdClass = Replace::preg('~^[RN](.*)$~', 'right-align $1', $tdClass);
+                    $tdClass = trim($tdClass);
+
+                    if ($tdClass === 'f') {
+                        $tdClass = '';
+                    }
+
+                    if (mb_strlen($tdClass) > 0) {
                         $cell->setAttribute('class', $tdClass);
                     }
-                    $colspan = 1;
-                    for ($k = $totalColSpan + 1; $k < count($thisRowFormat); ++$k) {
-                        if (@$thisRowFormat[$k] === 's') {
-                            ++$colspan;
-                        } else {
-                            break;
-                        }
+                    $thisColspan = 1;
+                    while (in_array(@$thisRowFormat[$totalColSpan + $thisColspan + 1],
+                        ['s', 'S'])) { // While we span right
+                        ++$thisColspan;
                     }
-                    if ($colspan > 1) {
-                        $cell->setAttribute('colspan', (string)$colspan);
+                    if ($thisColspan > 1) {
+                        $cell->setAttribute('colspan', (string)$thisColspan);
                     }
-                    $totalColSpan += $colspan;
+                    $totalColSpan += $thisColspan;
 
                     $cell = $tr->appendChild($cell);
 
