6a0ee064c4a4b0eaaaaa09ee84b74b0414d01e9d
Jackson Pauls <jacksonpauls@gmail.com>
2017-04-05 08:20:16 +0100

    Handle \z

--- lib/Preprocessor.php
+++ lib/Preprocessor.php
@@ -27,8 +27,12 @@ class Preprocessor
             // Don't worry about changes in point size for now (see rc.1 for digit instead of +- in \s10):
             $line = Replace::preg('~(?<!\\\\)((?:\\\\\\\\)*)\\\\s[-+\d]?\d~u', '$1', $line);
 
-            // Don't worry colour changes:
+            // Don't worry about colour changes:
             $line = Replace::preg('~(?<!\\\\)((?:\\\\\\\\)*)\\\\m(\(..|\[.*?\])~u', '$1', $line);
+
+            // Don't worry about:  \zc - Print c with zero width (without spacing).
+            // TODO: see if we can use this for underlining when \z_, e.g. in groff.7
+            $line = Replace::preg('~(?<!\\\\)((?:\\\\\\\\)*)\\\\z~u', '$1', $line);
 
             $linesNoComments[] = $line;
 
