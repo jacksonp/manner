f85706d025468579e8b318e892f88da4c53cfcdd
Jackson Pauls <jacksonpauls@gmail.com>
2017-04-05 22:17:02 +0100

    Handle .RE 1 specially

--- lib/Block/RE.php
+++ lib/Block/RE.php
@@ -1,6 +1,13 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
+/**
+ * .RE [nnn]
+ * This macro moves the left margin back to level nnn, restoring the previous left margin. If no argument is given, it
+ * moves one level back. The first level (i.e., no call to .RS yet) has number 1, and each call to .RS increases the
+ * level by 1.
+ *
+ */
 class Block_RE implements Block_Template
 {
 
@@ -13,6 +20,12 @@ class Block_RE implements Block_Template
     {
 
         array_shift($lines);
+
+
+        if (count($request['arguments']) && $request['arguments'][0] === '1') {
+            // 1 is back to base level, handle that specially as it is used e.g. in lsmcli.1
+            return $parentNode->ancestor('section');
+        }
 
         $lastIndentedBlock = $parentNode->ancestor('div');
         if (is_null($lastIndentedBlock)) {
