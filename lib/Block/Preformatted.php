e1e5d99ce096f75f46130400a81b864d2bedd35b
Jackson Pauls <jacksonpauls@gmail.com>
2017-08-21 20:39:44 +0100

    Comment.

--- lib/Block/Preformatted.php
+++ lib/Block/Preformatted.php
@@ -1,6 +1,29 @@
 <?php
 declare(strict_types=1);
 
+// TODO: handle this differently? Use a flag for preformat?
+// See esp. submit.1:
+/*
+ * environment variable \fBSGE_TASK_ID\fP. The option arguments n, m and s will be available through the environment variables \fBSGE_TASK_FIRST\fP, \fBSGE_TASK_LAST\fP and  \fBSGE_TASK_STEPSIZE\fP.
+.sp 1
+.nf
+.RS
+Following restrictions apply to the values n and m:
+.sp 1
+.RS
+1 <= n <= MIN(2^31-1, max_aj_tasks)
+1 <= m <= MIN(2^31-1, max_aj_tasks)
+n <= m
+.RE
+.fi
+.sp 1
+\fImax_aj_tasks\fP is defined in the cluster configuration (see
+.M sge_conf 5)
+.sp 1
+The task id range
+ *
+ */
+
 class Block_Preformatted implements Block_Template
 {
 
@@ -14,7 +37,7 @@ class Block_Preformatted implements Block_Template
 
         array_shift($lines);
 
-        if (Node::isOrInTag($parentNode,'pre') || !count($lines)) {
+        if (Node::isOrInTag($parentNode, 'pre') || !count($lines)) {
             return null;
         }
 
