5ec4fd83c69345c46e113fa09141bb9ea279f901
Jackson Pauls <jacksonpauls@gmail.com>
2017-09-03 11:01:47 +0100

    Remove commented-out and unused code from class Inline_ft

--- lib/Inline/ft.php
+++ lib/Inline/ft.php
@@ -4,14 +4,6 @@ declare(strict_types=1);
 class Inline_ft implements Block_Template
 {
 
-    private static function getNonFontParent(DOMElement $parentNode): DOMElement
-    {
-        while (in_array($parentNode->tagName, Blocks::INLINE_ELEMENTS)) {
-            $parentNode = $parentNode->parentNode;
-        }
-        return $parentNode;
-    }
-
     static function checkAppend(
         DOMElement $parentNode,
         array &$lines,
@@ -31,57 +23,6 @@ class Inline_ft implements Block_Template
         }
 
         return null;
-        /*
-
-        $fontAbbreviation = $request['arguments'][0];
-
-        // Skip stray regular font settings:
-        if (in_array($fontAbbreviation, ['0', '1', 'R', 'P', 'CR', 'AR'])) {
-            return self::getNonFontParent($parentNode);
-        }
-
-        $parentNode = Blocks::getParentForText($parentNode);
-
-        $dom = $parentNode->ownerDocument;
-
-        switch ($fontAbbreviation) {
-            case 'I':
-            case '2':
-            case 'AI':
-                if (Node::isOrInTag($parentNode, 'em')) {
-                    return null;
-                }
-                $node = $dom->createElement('em');
-                break;
-            case 'B':
-            case '3':
-                if (Node::isOrInTag($parentNode, 'strong')) {
-                    return null;
-                }
-                $node = $dom->createElement('strong');
-                break;
-            case 'C':
-            case 'CW':
-            case '4':
-            case '5':
-            case 'tt':
-            case 'CB':
-            case 'CS': // e.g. pmwebd.1
-                if (Node::isOrInTag($parentNode, 'code')) {
-                    return null;
-                }
-                $node = $dom->createElement('code');
-                break;
-            default:
-                $node = $dom->createElement('span');
-                $node->setAttribute('class', 'font-' . $fontAbbreviation);
-        }
-
-        $node = $parentNode->appendChild($node);
-
-        return $node;
-
-        */
 
     }
 
