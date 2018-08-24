1c95a2e316b5dd9bf00c537ef0337288446f0cdb
Jackson Pauls <jacksonpauls@gmail.com>
2018-08-24 17:13:55 +0100

    Formatting + fix issue with first negative number in expression not being matched.

--- lib/Roff/Unit.php
+++ lib/Roff/Unit.php
@@ -8,29 +8,29 @@ class Roff_Unit
 
     private const unitMultipliers = [
         // device dependent measurement, quite small, ranging from 1/75th to 1/72000th of an inch
-        'u' => 1,
+      'u' => 1,
         // inch
-        'i' => self::basicUnitsPerInch,
+      'i' => self::basicUnitsPerInch,
         // One inch is equal to 2.54cm.
-        'c' => self::basicUnitsPerInch / 2.54,
+      'c' => self::basicUnitsPerInch / 2.54,
         // Points. This is a typesetter’s measurement used for measure type size. It is 72 points to an inch.
-        'p' => self::basicUnitsPerInch / 72,
+      'p' => self::basicUnitsPerInch / 72,
         // Pica. Another typesetting measurement. 6 Picas to an inch (and 12 points to a pica).
-        'P' => self::basicUnitsPerInch / 6,
+      'P' => self::basicUnitsPerInch / 6,
         // https://www.gnu.org/software/groff/manual/html_node/Fractional-Type-Sizes.html#Fractional-Type-Sizes
-        's' => 1,
+      's' => 1,
         // https://www.gnu.org/software/groff/manual/html_node/Fractional-Type-Sizes.html#Fractional-Type-Sizes
-        'z' => 1,
+      'z' => 1,
         // https://www.gnu.org/software/groff/manual/html_node/Colors.html#Colors
-        'f' => 65536,
+      'f' => 65536,
         // em = 11/64 inch
-        'm' => self::basicUnitsPerInch * 11 / 64,
+      'm' => self::basicUnitsPerInch * 11 / 64,
         // en = 1/2 em:
-        'n' => (self::basicUnitsPerInch * 11 / 64) / 2,
+      'n' => (self::basicUnitsPerInch * 11 / 64) / 2,
         // By default, gtroff uses 10 point type on 12 point spacing. https://www.gnu.org/software/groff/manual/html_node/Sizes.html#Sizes
-        'v' => self::basicUnitsPerInch * 10 / 72,
+      'v' => self::basicUnitsPerInch * 10 / 72,
         // 100ths of an em.
-        'M' => (self::basicUnitsPerInch * 11 / 64) / 100,
+      'M' => (self::basicUnitsPerInch * 11 / 64) / 100,
     ];
 
     /**
@@ -45,16 +45,17 @@ class Roff_Unit
      */
     static function normalize(string $string, string $defaultUnit, string $targetUnit): string
     {
-
         $string = Replace::pregCallback(
-            '~((?:\d*\.)?\d+)([uicpPszfmnvM])?~u',
-            function ($matches) use ($defaultUnit, $targetUnit) {
-                $unit       = @$matches[2] ?: $defaultUnit;
-                $basicUnits = self::unitMultipliers[$unit] * $matches[1];
-                return (string)round($basicUnits / self::unitMultipliers[$targetUnit]);
-            }, $string);
+          '~((?:\d*\.)?\d+)([uicpPszfmnvM])?~u',
+          function ($matches) use ($defaultUnit, $targetUnit) {
+              $unit       = @$matches[2] ?: $defaultUnit;
+              $basicUnits = self::unitMultipliers[$unit] * $matches[1];
+
+              return (string)round($basicUnits / self::unitMultipliers[$targetUnit]);
+          }, $string);
 
         $string = preg_replace('~[^\d\.\(\)\+\*/\-><= ]~', '', $string);
+//        $string = preg_replace('~[^\d\.\(\)\+\*/\-><= ?]~', '', $string);
 
         return self::evaluate($string);
 
@@ -76,8 +77,18 @@ class Roff_Unit
         // Remove parentheses around a lone number:
         $evaluatedString = Replace::preg('~\(((?:\d*\.)?\d+)\)~u', '$1', $string);
 
+//        if (preg_match('~^(?<left>.*)(?<op>[><]\?)(?<right>.*)$~u', $evaluatedString, $matches)) {
+//            $left  = self::evaluate($matches['left']);
+//            $right = self::evaluate($matches['right']);
+//            if ($matches['op'] === '>?') {
+//                return max($left, $right);
+//            } elseif ($matches['op'] === '<?') {
+//                return min($left, $right);
+//            }
+//        }
+
         // Evaluate first matched expression only, because gtroff has no operator precedence:
-        $evaluatedString = Replace::pregCallback('~(?:\d*\.)?\d+[-\+\*/](?:\d*\.)?\d+~u', function ($matches) {
+        $evaluatedString = Replace::pregCallback('~[-\+]?(?:\d*\.)?\d+[-\+\*/](?:\d*\.)?\d+~u', function ($matches) {
             $expression = $matches[0];
             try {
                 return eval('return ' . $expression . ';');
