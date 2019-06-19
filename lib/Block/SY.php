65fba1a66342b29af24e5708651e7a37a59254f5
Jackson Pauls <jacksonpauls@gmail.com>
2019-06-19 21:25:29 +0100

    Render .SY as <table>

--- lib/Block/SY.php
+++ lib/Block/SY.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 class Block_SY implements Block_Template
 {
@@ -13,38 +13,64 @@ class Block_SY implements Block_Template
      * @throws Exception
      */
     static function checkAppend(
-        DOMElement $parentNode,
-        array &$lines,
-        array $request,
-        $needOneLineOnly = false
-    ): ?DOMElement
-    {
+      DOMElement $parentNode,
+      array &$lines,
+      array $request,
+      $needOneLineOnly = false
+    ): ?DOMElement {
 
-        array_shift($lines);
-
+        $dom        = $parentNode->ownerDocument;
         $parentNode = Blocks::getBlockContainerParent($parentNode, true);
 
-        if ($parentNode->tagName === 'pre') {
-            $parentNode = $parentNode->parentNode;
+        $foundEnd = false;
+
+        $syRows          = [];
+        $syLines         = [];
+        $lastCommandName = '';
+        $firstLine = true;
+        while ($request = Request::getLine($lines)) {
+            array_shift($lines);
+            if ($request['request'] === 'SY') {
+                $lastCommandName = count($request['arguments']) ? $request['arguments'][0] : '';
+                if (!$firstLine) { // Don't do this on first .SY
+                    $syRows[] = ['cmd_name' => $lastCommandName, 'sy_lines' => $syLines];
+                }
+                $syLines         = [];
+            } elseif ($request['request'] === 'YS') {
+                $syRows[] = ['cmd_name' => $lastCommandName, 'sy_lines' => $syLines];
+                $foundEnd = true;
+                break;
+            } else {
+                $syLines[] = $request['raw_line'];
+            }
+            $firstLine = false;
         }
 
-        $commandName = '';
-
-        if (count($request['arguments']) > 0) {
-            $commandName = $request['arguments'][0];
+        if (!$foundEnd) {
+            throw new Exception('SY without YS.');
         }
 
-        /* @var DomElement $pre */
-        $pre = $parentNode->ownerDocument->createElement('pre');
-        if ($commandName !== '') {
-            $commandName = trim(TextContent::interpretString($commandName));
-            $pre->setAttribute('class', 'synopsis');
-            $pre->appendChild(new DOMText($commandName . ' '));
+        $table = $dom->createElement('table');
+        $table->setAttribute('class', 'synopsis');
+
+        foreach ($syRows as $syRow) {
+            $tr            = $table->appendChild($dom->createElement('tr'));
+            $tdCommandName = $tr->appendChild($dom->createElement('td'));
+
+            if ($syRow['cmd_name'] !== '') {
+                $syRow['cmd_name'] = trim(TextContent::interpretString($syRow['cmd_name']));
+                $tdCommandName->appendChild(new DOMText($syRow['cmd_name']));
+            }
+
+            /* @var DomElement $tdOptions */
+            $tdOptions = $tr->appendChild($dom->createElement('td'));
+
+            Roff::parse($tdOptions, $syRow['sy_lines']);
         }
 
-        $pre = $parentNode->appendChild($pre);
+        $parentNode->appendChild($table);
 
-        return $pre;
+        return $parentNode;
 
     }
 
