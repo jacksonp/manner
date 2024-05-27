401db291f9a4a7e20b766e74040b1b7c26d513c9
Jackson Pauls <jacksonpauls@gmail.com>
2024-05-27 16:06:50 +0100

    Add COPYING and notices to each source file

--- lib/PreformattedOutput.php
+++ lib/PreformattedOutput.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * manner: convert troff man pages to semantic HTML
+ * Copyright (C) 2024  Jackson Pauls
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 declare(strict_types=1);
 
 namespace Manner;
@@ -96,7 +113,10 @@ class PreformattedOutput
             self::$addIndent = 0;
 
             return true;
-        } elseif ($request['raw_line'] === '' || in_array($request['class'], ['\Manner\Block\P', '\Manner\Inline\VerticalSpace'])) {
+        } elseif ($request['raw_line'] === '' || in_array(
+            $request['class'],
+            ['\Manner\Block\P', '\Manner\Inline\VerticalSpace']
+          )) {
             array_shift($lines);
             if ($parentNode->hasChildNodes()) {
                 $parentNode->appendChild(new DOMText("\n"));
@@ -114,7 +134,10 @@ class PreformattedOutput
             }
 
             return true;
-        } elseif (in_array($request['class'], ['\Manner\Inline\AlternatingFont', '\Manner\Inline\ft', '\Manner\Request\Skippable'])) {
+        } elseif (in_array(
+          $request['class'],
+          ['\Manner\Inline\AlternatingFont', '\Manner\Inline\ft', '\Manner\Request\Skippable']
+        )) {
             $request['class']::checkAppend($parentNode, $lines, $request);
             if ($request['class'] === '\Manner\Inline\AlternatingFont') {
                 self::endInputLine($parentNode);
